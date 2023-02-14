08ba8dbed9452ca4e6794752fe1b8f15f869579b
lisadimitrief <84084978+lisadimitrief@users.noreply.github.com>
2023-02-14 09:18:33 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const prenom = "Lisa"
+const nom = "Dimitrief-Dontcheff"
+var age = 23
+const phrase = "Bonjour je m'appel "+prenom+" "+nom+" et j'ai "+age+" ans."
+// window.alert(phrase);
+// const pseudo = "Lyrael "
+// const race = "dragon "
+// const niveau = 1
+// const role = " brute"
+// const pagePerso = pseudo+race+niveau+role
+// window.alert(pagePerso);
+
+//...
+// window.alert(new Date().toLocaleDateString("fr"));
+//...
+
+console.log(nom.length); //donne le nombre de caractère
+const monTableau = [];
+monTableau ["jaune avec des taches"] = "bibi";
+monTableau ["jaune"] = "bibu";
+monTableau ["noir"] = "bobo";
+console.table(monTableau);
+
+
