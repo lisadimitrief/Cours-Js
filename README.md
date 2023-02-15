226d39ee71ef0f7f00a3095e95bb889074cf293b
lisadimitrief <84084978+lisadimitrief@users.noreply.github.com>
2023-02-15 16:51:15 +0100

    continuations_cours_tableau

--- README.md
+++ README.md
@@ -53,3 +53,54 @@ Ce qui donne: ![3eme exemple](img/exemple_3.png)
 
 
 ***
+
+
+| Opérateurs de comparaison | Définition | Exemple |
+|:--------------|:------:|---------------:|
+| Egalité (==)| Renvoie true si les opérandes sont égaux (après conversion implicite). |"3" == 3 |
+| Inéglité (!=)| Renvoie true si les opérandes sont différents (après conversion implicite). | 3 !==4 |
+| Egalité sticte (===)| Renvoie true si les opérandes sont égaux et du même type. | 3 === '3' |
+| Inégalité stricte (!==)| Renvoie true si les opérandes sont du même type et différents ou s'ils ne sont pas du même type. | 3 !== "3" |
+| Supériorité stricte (>)| Renvoie true si l'opérande gauche est strictement supérieur à l'opérande droit. | "12" > 2 |
+| Supériorité (>=)| Renvoie true si l'opérande gauche est supérieur ou égal à l'opérande droit. | 3>= 4 |
+| Infériorité stricte (<) | Renvoie true si l'opérande gauche est strictement inférieur à l'opérande droit. | "2" < 12 |
+| Infériorité (<=) | Renvoie true si l'opérande gauche est inférieur ou égal à l'opérande droit. | 2 <= 5 |
+
+| Opérateurs arithmétiques | Définition | Exemple |
+|:--------------|:------:|---------------:|
+| Reste (%) | Un opérateur binaire qui renvoie le reste entier de la division des deux opérandes. | 12 % 5 renvoie 00 |
+| Incrément (++) | Un opérateur unaire qui ajoute un à son opérande. S'il est utilisé en opérateur préfixe (++x), il renvoie la valeur de son opérande après y avoir ajouté un. S'il est utilisé en opérateur postfixe (x++), il renvoie la valeur de l'opérande avant l'ajout de un. | Si x vaut 3, alors ++x définit x avec 4 et renvoie 4, tandis que x++ renvoie 3 puis, uniquement après, définit x avec 4. |
+| Décrément (--) | Un opérateur unaire qui soustrait un à son opérande. La valeur de retour est analogue à celle de l'opérateur d'incrément. | Si x vaut 3, alors --x définit x avec 2 et renvoie 2, tandis que x-- renvoie 3 puis, uniquement après, définit x avec 2. |
+| Négation unaire (-) | Un opérateur unaire qui renvoie l'opposé de l'opérande. | Si x vaut 3, -x renvoie -3. |
+| Plus unaire (+) | Un opérateur unaire qui tente la conversion de l'opérande en nombre si ce n'est pas déjà une valeur numérique. | +"3" renvoie 3. +true renvoie 1. |
+| Opérateur d'exponentiation (**) | Élève une base donnée par l'opérande gauche à la puissance donnée par l'opérande droit. | 2 ** 3 renvoie 8. 10 ** -1 renvoie 0.1. |
+
+| Opérateurs binaires | [cours](https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Expressions_and_Operators#op%C3%A9rateurs_binaires) | ø |
+|:--------------:| :------:|:---------------:|
+
+***
+
+var arrayNbr = [1, 2, 3];
+var arrayStr = ["a", "b", "c"];
+
+
+// arrayNbr.push(4); //[1, 2, 3, 4] ajoute
+// arrayNbr.pop(); //[1, 2] retire le dernier
+// arrayNbr.shift(); //[2, 3] retire le premier
+// arrayNbr.unshift(0); //[0, 1, 2, 3] ajoute la valeur indiqué au début
+// console.table(arrayNbr); //affiche le tableau dans la console
+//...
+// arrayStr.concat("d"); //fusionne des tableaux sans changer les valeurs du tableau initial
+// arrayStr.joint("-"); // a-b-c joindre les valeurs du tableau sans changer les valeurs du tableau initial
+// arrayStr.slice(2); // ["b", "c"] retire le nombre d'elements indiqué du tableau en partant du début sans changer les valeurs du tableau initial
+// arrayStr.slice(-1); // ["b", "c"] retire le nombre d'elements indiqué du tableau en partant du début et en comptant depuis la fin sans changer les valeurs du tableau initial
+// arrayStr.includes("c"); //regarde si "c" est inclu dans le tableau et envoi un boolean
+// arrayStr.indexOf("c"); // donne l'index de la valeur du tableau
+// arrayNbr.reduce((acc, cou)=> acc + cou) // 6 additions  
+//arrayNbr.find(el => el > 2) //3 renvoie la valeur du premier élément trouvé dans le tableau qui respecte la condition
+//arrayNbr.findIndex(el => el > 2) //2 renvoie l'index du premier élément trouvé dans le tableau qui respecte la condition
+// arrayNbr.map(el => el * 2); //[2, 4, 6] crée un nouveau tableau avec les résultats de l'appel d'une fonction fournie sur chaque élément du tableau appelant.
+// arrayNbr.some(el => el > 2); //true passe le test implémenté par la fonction fournie. Elle renvoie un booléen indiquant le résultat du test.
+// arrayNbr.filter(el => el > 1) // [2,3] renvoi tous les éléments qui respectent la condition
+// arrayNbr.every(el => el > 1);
+// arrayNbr.reverse();
--- index.js
+++ index.js
@@ -33,32 +33,181 @@ var maChaine = new String("exemple");
 
 //alert("ma variable machaine est de type : "+typeof(machaine)+ "\nma variable maChaine est de type : "+typeof(maChaine));
 
-var n = null;
-var u = undefined;
-var nn = NaN;
+// var n = null;
+// var u = undefined;
+// var nn = NaN;
 
 //alert("ma variable n est de type : "+typeof(n)+ "\nma variable u est de type : "+typeof(u)+"\nma variable nn est de type : "+typeof(nn));
 
-var heure_ouverture = 09.00;
-var heure_fermeture = 17.00;
-var actu = 9.30;
-var matin = 12.00;
+//...
 
-if (actu<heure_ouverture)
-{
-    alert("c'est trop tôt, revenez plus tard");
-}
-else if (actu>heure_ouverture && actu<heure_fermeture)
-{
-    if(actu>=heure_ouverture && actu<matin)
-    {
-        alert("Bienvenu ! C'est le matin !");
-    }
-    else
-    {
-    alert("Bienvenu ! C'est l'après-midi !");
-    }
-}
-else{
-    alert("Revenez demain");
-}
+// var heure_ouverture = 09.00;
+// var heure_fermeture = 17.00;
+// var actu = 09.00;
+// var matin = 12.00;
+
+// if (actu<heure_ouverture)
+// {
+//     alert("c'est trop tôt, revenez plus tard");
+// }
+// else if (actu>=heure_ouverture && actu<heure_fermeture)
+// {
+//     if(actu>=heure_ouverture && actu<matin)
+//     {
+//         alert("Bienvenu ! C'est le matin !");
+//     }
+//     else
+//     {
+//     alert("Bienvenu ! C'est l'après-midi !");
+//     }
+// }
+// else{
+//     alert("Revenez demain");
+// }
+
+//...
+// var note = 14;
+// var noteAmericaine ="";
+
+// if (note > 19) {
+//     noteAmericaine = "A+";
+// } else if (note >= 18 && note <=19) {
+//     noteAmericaine = "A";
+// }
+// } else if (note >= 15 && note <18) {
+//     noteAmericaine = "B";
+// }
+// } else if (note >= 12 && note <15) {
+//     noteAmericaine = "C";
+// }
+// } else if (note >= 9 && note <12) {
+//     noteAmericaine = "D";
+// }
+// } else if (note >= 6 && note <9) {
+//     noteAmericaine = "E";
+// }
+// } else if(note <6) {
+//     noteAmericaine = "F";
+// }
+// else{
+//     noteAmericaine = "il sèche purée !";
+// }
+// alert(noteAmericaine);
+
+// switch (noteAmericaine) {
+//     case "A+":
+//         alert("Magnifique")
+//         break;
+//     case "A":
+//         alert("Excellent Travail")
+//         break;
+//     case "B":
+//         alert("Très bon Travail")
+//         break;
+//     case "C":
+//         alert("En bonne voie")
+//         break;
+//     case "D":
+//         alert("Beaucoup de difficultés")
+//         break;
+//     case "E":
+//         alert("N'a pas compris le cours")
+//         break;
+//     case "F":
+//         alert("As tu lu les énoncés ?")
+//         break;
+        
+//     default:
+//         alert("aucune idée")
+//         break;
+//     }
+//...
+
+
+var facture = 500;
+var réduction = 0;
+
+// if (facture<1000)
+// {
+//     alert("Vous devez payer"+facture+"€");
+// }
+// else if(facture>=1000 && facture<=2500)
+// {
+//     facture = facture*0.95;
+//     rédiction = 5;
+//     alert("Vous devez payer"+facture+"€");
+// }
+// else(facture>2500)
+// {
+//     facture = facture*0.9;
+//     réduction = 10;
+//     alert("Vous devez payer"+facture+"€");
+// }
+// switch (réduction)
+// {
+//     case 0:
+//         alert("pas de réduction")
+//         break;
+//     case 5:
+//         alert("5% de réduction sur votre achat !")
+//         break;
+//     case 10:
+//         alert("10% de réduction sur votre achat !")
+//         break;    
+// }
+
+//vertion prof
+
+// if (facture < 1000){
+//     réduction = 0;
+// }
+// else if (facture <=2500 && facture >=1000){
+//     réduction = 5;
+// }
+// else {
+//     réduction = 10;
+// }
+
+// var total = facture-((réduction/100)*facture);
+
+// switch (réduction){
+//     case 0:
+//         console.log("autres", facture);
+//         break;
+//     default:
+//         console.log("total : ", total);
+//         break;
+// }
+
+//...
+
+var arrayNbr = [1, 2, 3];
+var arrayStr = ["a", "b", "c"];
+
+
+// arrayNbr.push(4); //[1, 2, 3, 4] ajoute
+// arrayNbr.pop(); //[1, 2] retire le dernier
+// arrayNbr.shift(); //[2, 3] retire le premier
+// arrayNbr.unshift(0); //[0, 1, 2, 3] ajoute la valeur indiqué au début
+// console.table(arrayNbr); //affiche le tableau dans la console
+//...
+// arrayStr.concat("d"); //fusionne des tableaux sans changer les valeurs du tableau initial
+// arrayStr.joint("-"); // a-b-c joindre les valeurs du tableau sans changer les valeurs du tableau initial
+// arrayStr.slice(2); // ["b", "c"] retire le nombre d'elements indiqué du tableau en partant du début sans changer les valeurs du tableau initial
+// arrayStr.slice(-1); // ["b", "c"] retire le nombre d'elements indiqué du tableau en partant du début et en comptant depuis la fin sans changer les valeurs du tableau initial
+// arrayStr.includes("c"); //regarde si "c" est inclu dans le tableau et envoi un boolean
+// arrayStr.indexOf("c"); // donne l'index de la valeur du tableau
+// arrayNbr.reduce((acc, cou)=> acc + cou) // 6 additions  
+//arrayNbr.find(el => el > 2) //3 renvoie la valeur du premier élément trouvé dans le tableau qui respecte la condition
+//arrayNbr.findIndex(el => el > 2) //2 renvoie l'index du premier élément trouvé dans le tableau qui respecte la condition
+// arrayNbr.map(el => el * 2); //[2, 4, 6] crée un nouveau tableau avec les résultats de l'appel d'une fonction fournie sur chaque élément du tableau appelant.
+// arrayNbr.some(el => el > 2); //true passe le test implémenté par la fonction fournie. Elle renvoie un booléen indiquant le résultat du test.
+// arrayNbr.filter(el => el > 1) // [2,3] renvoi tous les éléments qui respectent la condition
+// arrayNbr.every(el => el > 1);
+// arrayNbr.reverse();
+
+
+
+
+
+console.log(arrayCopy);
